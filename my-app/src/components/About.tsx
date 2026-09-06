0620403a0858b2ed42068f5cf14231d80a316328
Mayowa-de <mayortech3@gmail.com>
2026-09-06 06:33:54 -0700

    adjust the element and style of both  About.jsx and Project.jsx components

--- my-app/src/components/About.tsx
+++ my-app/src/components/About.tsx
@@ -4,9 +4,12 @@ import Profilepic from '../assets/profile.png'
 
 export default function About() {
   return (
-    <section className="min-h-screen w-full rounded-md backdrop-blur-xl border shadow border-[#0d1117] flex flex-col gap-10  px-4 mt-12 md:grid md:grid-cols-3  ">
-      <div className=" bg-transparent rounded shadow shadow-[#cf6223] border-[1px] border-[#cf6223] flex items-center justify-center backdrop-blur-xl w-full">
-        <img src={Profilepic} alt="profile-image"  className=""/>
+    <section className="min-h-screen w-full rounded-md backdrop-blur-xl  shadow bg-[#030a13]  flex flex-col gap-10 p-2 px-4 mt-12 md:grid md:grid-cols-3 justify-center items-center">
+      <div className="flex relative w-full">
+      <div className=" bg-transparent rounded-2xl shadow shadow-[#cf6223] border-[1px] border-[#cf6223] flex items-center justify-center backdrop-blur-xl w-fulll md:h-[400px] ">
+        <img src={Profilepic} alt="profile-image"  width={250} height={150} />
+      </div>
+      <h2 className="absolute bottom-10 text-2xl -right-4 text-[#cf6223] ">Mayor</h2>
       </div>
       <div className=" flex flex-col gap-8 lg:gap-4 w-full">
       <span className="text-[#cf6223] font-semibold">ABOUT ME</span>
@@ -17,13 +20,14 @@ export default function About() {
         </h3>
       </div>
       <p className="text-slate-500 targeting-[1.5px] text-[1rem] md:text-[1rem] lg:text-[1rem] leading-[2]">
-        I'm Mayowa Ojomu a FullStack Developer with passion for creating
+        I'm a FullStack Developer with passion for creating
         performant, user-friendly applications. I focus on clean code, intuitive
         design, and delivering solutions that make a difference.
       </p>
 
-      <div className="flex gap-8 text-[#cf6223] justify-center lg:justify-start mt-4 p-4 flex-col lg:shadow-none shadow-[#cf6223] shadow border-[#cf6223] rounded-xl lg:grid lg:grid-cols-4">
-        <div className="flex gap-10 lg:gap-6 lg:grid lg:grid-cols-1 justify-start" >
+      <div className="flex gap-8 text-[#cf6223] bg-[#0d1117]/50 justify-center lg:justify-start mt-4 p-4 flex-col lg:shadow-none shadow-[#cf6223] shadow border-[#cf6223] rounded-xl lg:grid lg:grid-cols-4">
+        <div className="flex gap-10 lg:gap-6 lg:grid lg:grid-cols-1 justify-start border-b-[1px] 
+        pb-4 border-b-gray-400/5 " >
           <div className="p-4 rounded-full border-none shadow shadow-[#cf6223] flex items-center  lg:shadow-none">
           <BriefcaseBusiness/>
           </div>
@@ -32,7 +36,8 @@ export default function About() {
             <p className="text-slate-500">Years Experience</p>
           </div>
         </div>
-        <div className="flex gap-10 lg:gap-6 lg:grid lg:grid-cols-1 "> 
+        <div className="flex gap-10 lg:gap-6 lg:grid lg:grid-cols-1 border-b-[1px] 
+        pb-4 border-b-gray-400/5"> 
           <div className="p-4 rounded-full border-none shadow-sm lg:shadow-none shadow-[#cf6223] flex items-center">
           <Package/>
           </div>
@@ -41,7 +46,8 @@ export default function About() {
             <p className="text-slate-500">Projects Completed</p>
           </div>
         </div>
-        <div className="flex gap-10 lg:gap-6 lg:grid lg:grid-cols-1 ">
+        <div className="flex gap-10 lg:gap-6 lg:grid lg:grid-cols-1 border-b-[1px] 
+        pb-4 border-b-gray-400/5">
           <div className="p-4 rounded-full border-none shadow-sm shadow-[#cf6223] flex items-center lg:shadow-none">
           <ShieldUser/>
           </div>
@@ -50,7 +56,8 @@ export default function About() {
             <p className="text-slate-500">Happy Clients</p>
           </div>
         </div>
-         <div className="flex gap-10  lg:gap-6 lg:grid lg:grid-cols-1 ">
+         <div className="flex gap-10  lg:gap-6 lg:grid lg:grid-cols-1 border-b-[1px] 
+        pb-4 border-b-gray-400/5">
           <div className="p-4 rounded-full border-none shadow-sm shadow-[#cf6223] items-center flex lg:shadow-none">
           <Atom />
           </div>
@@ -62,34 +69,35 @@ export default function About() {
       </div>
       </div>
 
-      <div className="flex flex-col w-full">
-        <div className="flex flex-col gap-2 border border-[#0d1117] shadow p-2 px-2">
+      <div className="flex flex-col w-full gap-5">
+        <div className="flex flex-col gap-2 border border-gray-500/50 rounded-xl  p-4 px-4 shadow-inner">
     
           <p className="text-sky-400">const <span className="text-purple-400">developer</span> <span className="text-white">= {"{"}</span></p>
           <div className="ml-4 flex flex-col gap-2">
-          <p className="text-sky-400">name: <span className="text-green-400">'Mayor Tech',</span></p>
+          <p className="text-sky-400">name: <span className="text-green-400">'Mayowa Ojomu',</span></p>
           <p className="text-sky-400">role: <span className="text-green-400">'Full Stack Developer',</span></p>
           <p className="text-sky-400">skills: <span className="text-green-400">{"['React','Typescript','Node.js']"},</span></p>
           <p className="text-sky-400">passion: <span className="text-green-400">'Building products that matter',</span></p>
           </div>
           <span>{"}"}</span>
         </div>
-        <div>
-         <div className="flex">
-          <FiCode/>
+
+        <div className="flex flex-col gap-2">
+         <div className="flex items-center gap-4">
+          <FiCode className="text-[#cf6223] text-xl"/>
           <p>Clean and maintainable</p>
          </div>
-         <div className="flex">
-          <FiTarget/>
-          <p>Clean and maintainable</p>
+         <div className="flex items-center gap-4">
+          <FiTarget className="text-[#cf6223] text-xl"/>
+          <p>Peformance Focused</p>
          </div>
-         <div className="flex">
-          <FiCheckCircle/>
-          <p>Clean and maintainable</p>
+         <div className="flex items-center gap-4">
+          <FiCheckCircle className="text-[#cf6223] text-xl"/>
+          <p>Pixel Perfect Design</p>
          </div>
-         <div className="flex">
-          <FiHeart/>
-          <p>Clean and maintainable</p>
+         <div className="flex items-center gap-4">
+          <FiHeart className="text-[#cf6223] text-xl"/>
+          <p>Scalable Architecture</p>
          </div>
         </div>
       </div>
--- my-app/src/components/Project.tsx
+++ my-app/src/components/Project.tsx
@@ -1,6 +1,6 @@
 import Ecommerce from '../assets/ecommerce1 (2).png'
 import Blog from '../assets/blog1.png'
-import Browser from '../assets/Browser.PNG'
+
 
 export default function Project() {
   return (
@@ -8,32 +8,37 @@ export default function Project() {
         <h4 className='text-[#cf6223]'>FEATURED WORK</h4>
 
         <p>Projects I'm Proud Of</p>
-        <div className='flex flex-col gap-6 md:grid md:grid-cols-4'>
-        <div className='flex flex-col gap-4 rounded shadow'>
+        <div className='flex flex-col gap-6 md:grid md:grid-cols-4 '>
+        <div className='flex flex-col gap-4 rounded shadow bg-[#030a13] pb-4 '>
+    
             <img src={Ecommerce} alt="" />
+            <div className='flex flex-col gap-2 px-4'>
             <h4>Ecommerce website</h4>
             <p>
 
             </p>
-            <ul className='flex gap-4'>
+            <ul className='flex gap-4 text-sm'>
                 <li>REACT</li>
                 <li>Tailwindcss</li>
                 <li></li>
             </ul>
+            </div>
         </div>
-        <div className='flex flex-col gap-4 rounded shadow'>
+        <div className='flex flex-col gap-4 rounded shadow bg-[#030a13] pb-4'>
             <img src={Blog} alt="" />
+            <div className='flex flex-col gap-2 px-4'>
             <h4>Blog website</h4>
             <p>
 
             </p>
-            <ul className='flex gap-4'>
+            <ul className='flex gap-4 text-sm'>
                 <li>REACT</li>
                 <li>Tailwindcss</li>
                 <li></li>
             </ul>
+            </div>
         </div>
-        <div className='flex flex-col gap-4 rounded shadow'>
+        {/* <div className='flex flex-col gap-4 rounded shadow'>
             <img src={Browser} alt="" />
             <h4>Browser website</h4>
             <p>
@@ -44,7 +49,7 @@ export default function Project() {
                 <li>Tailwindcss</li>
                 <li></li>
             </ul>
-        </div>
+        </div> */}
         </div>
     </section>
   )
